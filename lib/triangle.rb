2f92200a353454db95dcad005d14bd55e1da62d7
Brock Byrd <brockbyrdd@gmail.com>
2020-09-23 21:59:10 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -5,10 +5,23 @@ class Triangle
     @length_one = length_one
     @length_two = length_two
     @length_three = length_three
+    endone
+  def kind
+    if (@length_one <= 0) || (@length_two <= 0) || (@length_three <= 0)
+      raise TriangleError
+    elsif (@length_one+@length_two <= @length_three) || (@length_1+@length_three <= @length_two) || (@length_two+@length_three <= @length_one)
+      raise TriangleError
+    else
+      if (@length_one == @length_two) && (@length_two == @length_three)
+        :equilateral
+      elsif (@length_one == @length_two) || (@length_two == @length_three) || (@length_1 == @length_three)
+        :isosceles
+      elsif (twolength_one != @length_two) && (@length_two != @length_three) && (@length_1 != @length_three)
+        :scalene
+      end
     end
 
-  def kind(type)
-    type
+end
   end
 
   class TriangleError < StandardError
